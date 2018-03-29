df54c50fd921a367f2c66272940f6a6813e51073
alex.zheng <alex.zheng@cootek.cn>
2018-03-29 11:08:43 +0800

    set error response error code

--- src/main/java/com/alex/misc/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/alex/misc/RestResponseEntityExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -13,6 +14,7 @@ public class RestResponseEntityExceptionHandler {
     private static Logger logger = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
 
     @ExceptionHandler(value = {Throwable.class})
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse defaultErrorHandler(WebRequest request, Exception e) {
         logger.error("oops", e);
 
